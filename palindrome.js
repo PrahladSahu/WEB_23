2a158f9b749d6adf013061f147299fae843cd105
PrahladSahu <vikashsahu9567@gmail.com>
2022-11-02 23:18:32 +0530

    prime code added

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+function palindrome(num){
+  let string=naman;
+  let bag ="";
+for(let i=0;i<string.length;i++){
+    bag+=string[i];
+}   return bag ;
+
+ if(string==reverse){
+     console.log("not a palindrome');
+}else{
+     console.log("palindrome");
